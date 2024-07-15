677663db352cbb78aaa4e18b9dd7d26df73b534d
Ximena Vera <43298520+lximenavera@users.noreply.github.com>
2024-07-15 16:31:39 -0500

    Add files via upload

---
+++ modulo.js
@@ -0,0 +1,17 @@
+//EXPORT
+// export const name = "Pedro"
+// export const edad = "30"
+
+// const nombre = "Ximena"
+// const apell ="vera"
+// export{nombre, apell}
+
+//EXPORT DEFAULT
+
+const message =()=>{
+    const name = "Eduardo"
+    const materia = "Javascript"
+    return `${name} tiene que reforar la siguiente materia: ${materia}`
+}
+
+export default message
